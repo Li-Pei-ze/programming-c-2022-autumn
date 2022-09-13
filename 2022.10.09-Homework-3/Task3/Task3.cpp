2c59b7a82a05f50e2b26d268590c0ccf71e6bd11
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.10.09-Homework-3/Task3/Task3.cpp
@@ -0,0 +1,25 @@
+﻿// Task3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+	int n = 0;
+	std::cin >> n;
+	int i = 1;
+	int j = 1;
+	do
+	{
+		j = 1;
+		do
+		{
+			std::cout << i - 1 + j << " ";
+			++j;
+		} while (j <= n);
+		std::cout << std::endl;
+
+		++i;
+	} while (i <= n);
+	return EXIT_SUCCESS;
+}
