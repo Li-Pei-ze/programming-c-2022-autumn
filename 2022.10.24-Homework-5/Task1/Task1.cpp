0d6012dd94a1ea9d32e4701cc920472edba61303
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.10.24-Homework-5/Task1/Task1.cpp
@@ -0,0 +1,25 @@
+﻿// Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	int n;
+	std::cin >> n;
+	int a[1001];
+	for (int i = 1; i <= n; ++i)
+	{
+		std::cin >> a[i];
+	}
+	int x = 0;
+	std::cin >> x;
+	int tot = 0;
+	for (int i = 1; i <= n; ++i)
+	{
+		if (a[i] == x)
+			++tot;
+	}
+	std::cout << tot << std::endl;
+	return EXIT_SUCCESS;
+}
