68497bf262249848ccc061f054856c587d51ac67
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.09.15/Task4/Task4/Task4.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int speed, ti;
+int main()
+{
+    cin >> speed >> ti;
+    int ans = (speed * ti) % 109;
+    if (ans < 0) ans += 109;
+    cout << ans << endl;
+    return 0;
+}
