043fbcaf05315c00723c7cdce32dd17e627e5fa5
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.09.15-Homework-1/Task9/Task9.cpp
@@ -0,0 +1,15 @@
+﻿// Task9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+long long x;
+int main(int argc, char **argv)
+{
+	cin >> x;
+	long long a = x * x;
+	cout << a * (a + x + 1) + x + 1 << endl;
+	return EXIT_SUCCESS;
+}
