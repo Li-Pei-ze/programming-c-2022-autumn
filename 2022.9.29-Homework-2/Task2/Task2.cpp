1c7029ef18f9199e196479afff509419ce8145a2
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.9.29-Homework-2/Task2/Task2.cpp
@@ -0,0 +1,26 @@
+﻿// Task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int year;
+    std::cin >> year;
+
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+                std::cout << "YES" << std::endl;
+            else
+                std::cout << "NO" << std::endl;
+        }
+        else
+            std::cout << "YES" << std::endl;
+    }
+    else
+        std::cout << "NO" << std::endl;
+	return EXIT_SUCCESS;
+}
