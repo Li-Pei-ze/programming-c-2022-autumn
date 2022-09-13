3d9ff10c472424aef5d6f4c6cddb12ecbdf8ae07
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.09.15-Homework-1/Task7/Task7.cpp
@@ -0,0 +1,17 @@
+﻿// Task7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int a, b;
+	cin >> a >> b;
+	int t = a;
+	a = b;
+	b = t;
+	cout << a << b << endl;
+	return EXIT_SUCCESS;
+}
