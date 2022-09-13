e89cbaab9f76c46a3c776f645b0796a235d129b9
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.10.16-Homework-4/Task1/Task1.cpp
@@ -0,0 +1,21 @@
+﻿// Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	int n;
+	std::cin >> n;
+	int i = 1;
+	int sum = 0;
+	for (i = 1; i * i < n; ++i)
+	{
+		if (n % i == 0)
+		{
+			++sum;
+		}
+	}
+	std::cout << (sum * 2) + ((i * i) == n) << std::endl;
+	return EXIT_SUCCESS;
+}
