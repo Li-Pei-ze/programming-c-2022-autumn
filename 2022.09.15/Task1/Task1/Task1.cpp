a7b570410e8a72946ed5228648cf101d174f3558
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.09.15/Task1/Task1/Task1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+using namespace std;
+int n;
+int main()
+{
+	cin >> n;
+	cout << "The next number for the number " << n << " is " << n + 1 << endl;
+	cout << "The previous number for the number" << n << " is " << n-1<< endl;
+	return 0;
+}
