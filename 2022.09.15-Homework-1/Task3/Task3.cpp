2c4136a5da549ed1e9aa705403fb49d7c197f1d6
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.09.15-Homework-1/Task3/Task3.cpp
@@ -0,0 +1,12 @@
+﻿// Task3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int n, k;
+int main()
+{
+	cin >> n >> k;
+	cout << k % n << endl;
+	return 0;
+}
