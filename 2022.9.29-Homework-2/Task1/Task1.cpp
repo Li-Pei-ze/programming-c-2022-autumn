9b49618b5d67969bea0b06cf8c961ac011f61fb0
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

--- 2022.9.29-Homework-2/Task1/Task1.cpp
+++ 2022.9.29-Homework-2/Task1/Task1.cpp
@@ -1,8 +1,8 @@
 ﻿#include <iostream>
 
-int main(int argc, char* argv[])
+int main(int argc, char **argv)
 {
-    int n = 0;
+    int n;
 
     std::cin >> n;
 
@@ -11,232 +11,263 @@ int main(int argc, char* argv[])
         switch (n / 100)
         {
         case 1:
-            std::cout << "Сто бананов";
+            std::cout << "сто бананов";
             break;
 
         case 2:
-            std::cout << "Двести бананов";
+            std::cout << "двести бананов";
             break;
 
         case 3:
-            std::cout << "Триста бананов";
+            std::cout << "триста бананов";
             break;
 
         case 4:
-            std::cout << "Четыреста бананов";
+            std::cout << "четыреста бананов";
             break;
 
         case 5:
-            std::cout << "Пятьсот бананов";
+            std::cout << "пятьсот бананов";
             break;
 
         case 6:
-            std::cout << "Шестьсот бананов";
+            std::cout << "шестьсот бананов";
             break;
 
         case 7:
-            std::cout << "Семьсот бананов";
+            std::cout << "семьсот бананов";
             break;
 
         case 8:
-            std::cout << "Восемьсот бананов";
+            std::cout << "восемьсот бананов";
             break;
 
         case 9:
-            std::cout << "Девятьсот бананов";
+            std::cout << "девятьсот бананов";
             break;
+
         default:
             break;
         }
         exit(0);
     }
-
-    switch (n / 100)
+    else
     {
-    case 1:
-        std::cout << "Сто ";
-        break;
-
-    case 2:
-        std::cout << "Двести ";
-        break;
-
-    case 3:
-        std::cout << "Триста ";
-        break;
-
-    case 4:
-        std::cout << "Четыреста ";
-        break;
-
-    case 5:
-        std::cout << "Пятьсот ";
-        break;
-
-    case 6:
-        std::cout << "Шестьсот ";
-        break;
-
-    case 7:
-        std::cout << "Семьсот ";
-        break;
-
-    case 8:
-        std::cout << "Восемьсот ";
-        break;
-
-    case 9:
-        std::cout << "Девятьсот ";
-        break;
-    default:
-        break;
-    }
-
-    if (((n % 100) >= 10) && ((n % 100) < 20))
-    {
-        switch (n % 100)
+        switch (n / 100)
         {
-        case 10:
-            std::cout << "десять бананов";
+        case 1:
+            std::cout << "сто ";
             break;
 
-        case 11:
-            std::cout << "одиннадцать бананов";
+        case 2:
+            std::cout << "двести ";
             break;
 
-        case 12:
-            std::cout << "двенадцать бананов";
+        case 3:
+            std::cout << "триста ";
             break;
 
-        case 13:
-            std::cout << "тренадцать бананов";
+        case 4:
+            std::cout << "четыреста ";
             break;
 
-        case 14:
-            std::cout << "четырнадцать бананов";
+        case 5:
+            std::cout << "пятьсот ";
             break;
 
-        case 15:
-            std::cout << "пятнадцать бананов";
+        case 6:
+            std::cout << "шестьсот ";
             break;
 
-        case 16:
-            std::cout << "шестнадцать бананов";
+        case 7:
+            std::cout << "семьсот ";
             break;
 
-        case 17:
-            std::cout << "семьнадцать бананов";
+        case 8:
+            std::cout << "восемьсот ";
             break;
 
-        case 18:
-            std::cout << "восемнадцать бананов";
-            break;
-
-        case 19:
-            std::cout << "девятнадцать бананов";
+        case 9:
+            std::cout << "девятьсот ";
             break;
 
         default:
             break;
         }
-    }
-    else
-    {
-        if ((n % 10) == 0)
+
+        if (((n % 100) >= 10) && ((n % 100) < 20))
         {
-            switch ((n % 100) / 10)
+            switch (n % 100)
             {
-            case 2:
-                std::cout << "двадцать бананов";
+            case 10:
+                std::cout << "десять бананов";
                 break;
-            case 3:
-                std::cout << "тридцать бананов";
+
+            case 11:
+                std::cout << "одиннадцать бананов";
                 break;
-            case 4:
-                std::cout << "сорок бананов";
+
+            case 12:
+                std::cout << "двенадцать бананов";
                 break;
-            case 5:
-                std::cout << "пятьдесят бананов";
+
+            case 13:
+                std::cout << "тренадцать бананов";
                 break;
-            case 6:
-                std::cout << "шестьдесят бананрв";
+
+            case 14:
+                std::cout << "четырнадцать бананов";
                 break;
-            case 7:
-                std::cout << "семьдесят бананов";
+
+            case 15:
+                std::cout << "пятнадцать бананов";
                 break;
-            case 8:
-                std::cout << "восемьдесят бананов";
+
+            case 16:
+                std::cout << "шестнадцать бананов";
                 break;
-            case 9:
-                std::cout << "девяносто бананов";
+
+            case 17:
+                std::cout << "семьнадцать бананов";
                 break;
+
+            case 18:
+                std::cout << "восемнадцать бананов";
+                break;
+
+            case 19:
+                std::cout << "девятнадцать бананов";
+                break;
+
             default:
                 break;
             }
+        }
+        else
+        {
+            if ((n % 10) == 0)
+            {
+                switch ((n % 100) / 10)
+                {
+                case 2:
+                    std::cout << "двадцать бананов";
+                    break;
+
+                case 3:
+                    std::cout << "тридцать бананов";
+                    break;
+
+                case 4:
+                    std::cout << "сорок бананов";
+                    break;
+
+                case 5:
+                    std::cout << "пятьдесят бананов";
+                    break;
+
+                case 6:
+                    std::cout << "шестьдесят бананрв";
+                    break;
+
+                case 7:
+                    std::cout << "семьдесят бананов";
+                    break;
+
+                case 8:
+                    std::cout << "восемьдесят бананов";
+                    break;
+
+                case 9:
+                    std::cout << "девяносто бананов";
+                    break;
+
+                default:
+                    break;
+                }
             exit(0);
-        }
-        switch ((n % 100) / 10)
-        {
-        case 2:
-            std::cout << "двадцать ";
-            break;
-        case 3:
-            std::cout << "тридцать ";
-            break;
-        case 4:
-            std::cout << "сорок ";
-            break;
-        case 5:
-            std::cout << "пятьдесят ";
-            break;
-        case 6:
-            std::cout << "шестьдесят ";
-            break;
-        case 7:
-            std::cout << "семьдесят ";
-            break;
-        case 8:
-            std::cout << "восемьдесят ";
-            break;
-        case 9:
-            std::cout << "девяносто ";
-            break;
-        default:
-            break;
-        }
-        switch (n % 10)
-        {
-        case 1:
-            std::cout << "один банан";
-            break;
-        case 2:
-            std::cout << "два банана";
-            break;
-        case 3:
-            std::cout << "три банана";
-            break;
-        case 4:
-            std::cout << "четыре банана";
-            break;
-        case 5:
-            std::cout << "пять бананов";
-            break;
-        case 6:
-            std::cout << "шесть бананов";
-            break;
-        case 7:
-            std::cout << "семь бананов";
-            break;
-        case 8:
-            std::cout << "восемь бананов";
-            break;
-        case 9:
-            std::cout << "девять бананов";
-            break;
-        default:
-            break;
+            }
+            else
+            {
+                switch ((n % 100) / 10)
+                {
+                case 2:
+                    std::cout << "двадцать ";
+                    break;
+
+                case 3:
+                    std::cout << "тридцать ";
+                    break;
+
+                case 4:
+                    std::cout << "сорок ";
+                    break;
+
+                case 5:
+                    std::cout << "пятьдесят ";
+                    break;
+
+                case 6:
+                    std::cout << "шестьдесят ";
+                    break;
+
+                case 7:
+                    std::cout << "семьдесят ";
+                    break;
+
+                case 8:
+                    std::cout << "восемьдесят ";
+                    break;
+
+                case 9:
+                    std::cout << "девяносто ";
+                    break;
+
+                default:
+                    break;
+                }
+                switch (n % 10)
+                {
+                case 1:
+                    std::cout << "один банан";
+                    break;
+
+                case 2:
+                    std::cout << "два банана";
+                    break;
+
+                case 3:
+                    std::cout << "три банана";
+                    break;
+
+                case 4:
+                    std::cout << "четыре банана";
+                    break;
+
+                case 5:
+                    std::cout << "пять бананов";
+                    break;
+
+                case 6:
+                    std::cout << "шесть бананов";
+                    break;
+
+                case 7:
+                    std::cout << "семь бананов";
+                    break;
+
+                case 8:
+                    std::cout << "восемь бананов";
+                    break;
+
+                case 9:
+                    std::cout << "девять бананов";
+                    break;
+
+                default:
+                    break;
+                }
+            }
         }
     }
-
     return EXIT_SUCCESS;
 }
