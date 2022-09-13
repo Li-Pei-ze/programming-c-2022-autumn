2b8dc6c56e5f22f749d3a5eef2da088774703e9d
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.10.24-Homework-5/Task3/Task3.cpp
@@ -0,0 +1,36 @@
+﻿// Task3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	int n = 0;
+	int a[1001]{};
+	std::cin >> n;
+	for (int i = 1; i <= n; ++i)
+	{
+		std::cin >> a[i];
+	}
+	int maxnum = -INT_MAX;
+	int minnum = INT_MAX;
+	for (int i = 1; i <= n; ++i)
+	{
+		if (a[i] > maxnum)
+		{
+			maxnum = a[i];
+		}
+		if (a[i] < minnum)
+		{
+			minnum = a[i];
+		}
+	}
+	for (int i = 1; i <= n; ++i)
+	{
+		if (a[i] == maxnum)
+			a[i] = minnum;
+	}
+	for (int i = 1; i <= n; ++i)
+		std::cout << a[i] << " ";
+	return EXIT_SUCCESS;
+}
