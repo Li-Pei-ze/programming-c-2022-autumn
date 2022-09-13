68f1a4d91f42a935b4fc72cd474ea4c8922831f3
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.10.24-Homework-5/Additional-Task/Additional-Task.cpp
@@ -0,0 +1,28 @@
+﻿// Additional-Task.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char** argv)
+{
+	int f[101];
+	int a[101];
+	int n = 0;
+	std::cin >> n;
+	for (int i = 1; i <= n; ++i)
+	{
+		std::cin >> a[i];
+	}
+	memset(f, 0, sizeof f);
+	for (int i = 1; i <= n; ++i)
+		for (int j = 0; j < i; ++j)
+		{
+			if (f[j] + (i - j) * a[i] > f[i])
+			{
+				f[i] = f[j] + (i - j) * a[i];
+			}
+		}
+	std::cout << f[n]  << std::endl;
+	return EXIT_SUCCESS;
+}
