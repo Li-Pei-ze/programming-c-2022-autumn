019ec177d5c63181d34515d8f9825dbb916f7ffc
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

--- 2022.9.29-Homework-2/Task3/Task3.cpp
+++ 2022.9.29-Homework-2/Task3/Task3.cpp
@@ -5,5 +5,26 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int k = 0;
+	int m = 0;
+	int n = 0;
+
+	std::cin >> k >> m >> n;
+	if (n <= k)
+	{
+		std::cout << 2 * m << std::endl;
+	}
+	else
+	{
+		if ((n * 2) % k == 0)
+		{
+			std::cout << m * ((n * 2) / k) << std::endl;
+		}
+		else
+		{
+			std::cout << m * (1 + ((n * 2) / k)) << std::endl;
+		}
+	}
+
+	return EXIT_SUCCESS;
 }
