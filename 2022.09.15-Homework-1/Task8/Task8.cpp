3fb74a1cdde334c9b4a3a295a2fd0a41090ee862
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.09.15-Homework-1/Task8/Task8.cpp
@@ -0,0 +1,14 @@
+﻿// Task8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int h1, m1, s1, h2, m2, s2;
+int main()
+{
+	cin >> h1 >> m1 >> s1 >> h2 >> m2 >> s2;
+	cout << (h2 - h1) * 3600 + (m2 - m1) * 60 + s2 - s1 << endl;
+	return 0;
+}
