64f1e8c6db49f62686d2bed732f6379b91da2608
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.10.24-Homework-5/Task4/Task4.cpp
@@ -0,0 +1,25 @@
+﻿// Task4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	int n = 0;
+	int a[1001]{};
+	std::cin >> n;
+	for (int i = 1; i <= n; ++i)
+	{
+		std::cin >> a[i];
+	}
+	int maxnum = -INT_MAX;
+	for (int i = 2; i < n; ++i)
+	{
+		if (a[i] + a[i - 1] + a[i + 1] > maxnum)
+		{
+			maxnum = a[i] + a[i - 1] + a[i + 1];
+		}
+	}
+	std::cout << maxnum << std::endl;
+	return EXIT_SUCCESS;
+}
---
+++ 2022.10.24-Homework-5/Task5/Task5.cpp
@@ -0,0 +1,29 @@
+﻿// Task5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	int n = 0;
+	int a[1001]{};
+	std::cin >> n;
+	for (int i = 1; i <= n; ++i)
+	{
+		std::cin >> a[i];
+	}
+	int num = 0;
+	std::cin >> num;
+	int mindis = INT_MAX;
+	int minind = 0;
+	for (int i = 1; i <= n; ++i)
+	{
+		if (abs(num - a[i]) < mindis)
+		{
+			minind = i;
+			mindis = abs(num - a[i]);
+		}
+	}
+	std::cout << a[minind] << std::endl;
+	return EXIT_SUCCESS;
+}
