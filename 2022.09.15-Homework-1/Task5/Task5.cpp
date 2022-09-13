ab0bde8417a9f845e40201fd81ae3b79a1433f25
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.09.15-Homework-1/Task5/Task5.cpp
@@ -0,0 +1,12 @@
+﻿// Task5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include<iostream>
+using namespace std;
+int a;
+int main()
+{
+	cin >> a;
+	cout << (int)(a / 100) + (int)(a / 10) % 10 + a % 10 << endl;
+	return 0;
+}
---
+++ 2022.09.15-Homework-1/Task6/Task6.cpp
@@ -0,0 +1,12 @@
+﻿// Task6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int a;
+int main()
+{
+	cin >> a;
+	cout << a + (a + 1) % 2 + 1 << endl;
+	return 0;
+}
