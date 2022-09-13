0d1103b04d53f5550d9d84587d0599a6c8688a82
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.10.09-Homework-3/Task2/Task2.cpp
@@ -0,0 +1,21 @@
+﻿// Task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int x = 1;
+	int sum = 0;
+	while (x != 0)
+	{
+		if (x < 0)
+		{
+			++sum;
+		}
+		std::cin >> x;
+	}
+	std::cout << sum << std::endl;
+
+	return EXIT_SUCCESS;
+}
