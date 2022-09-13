d8edabba26b65351c5e32d3df298f1873c4d6edb
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.9.29-Homework-2/Task6/Task6.cpp
@@ -0,0 +1,25 @@
+﻿// Task6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    int x1 = 0;
+    int y1 = 0;
+    int x2 = 0;
+    int y2 = 0;
+
+    std::cin >> x1 >> y1 >> x2 >> y2;
+
+    if ((x1 == x2) || (y1 == y2) || (abs(x1 - x2) == abs(y1 - y2)))
+    {
+        std::cout << "YES" << std::endl;
+    }
+    else
+    {
+        std::cout << "NO" << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
