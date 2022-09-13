ca7aeb0c12b618fb1ab8f822afd48fa8902d90b3
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.10.09-Homework-3/Task1/Task1.cpp
@@ -0,0 +1,17 @@
+﻿// Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    std::cin >> a >> b;
+    while (a < b)
+    {
+        std::cout << a << " ";
+        ++a;
+    }
+    return EXIT_SUCCESS;
+}
