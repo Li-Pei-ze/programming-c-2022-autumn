767869443ca39b77ec4fec3ff71f7db6edc1e809
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.10.24-Homework-5/Task6/Task6.cpp
@@ -0,0 +1,32 @@
+﻿// Task6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	int n = 0;
+	int a[1001]{};
+	int pro[1001]{};
+	std::cin >> n;
+	for (int i = 1; i <= n; ++i)
+	{
+		std::cin >> a[i];
+	}
+	for (int i = 1; i <= n; ++i)
+	{
+		std::cin >> pro[i];
+	}
+	int maxnum = -INT_MAX;
+	int maxind = -1;
+	for (int i = 1; i <= n; ++i)
+	{
+		if (a[i] * pro[i] > maxnum)
+		{
+			maxnum = a[i];
+			maxind = i;
+		}
+	}
+	std::cout << maxind << std::endl;
+	return EXIT_SUCCESS;
+}
