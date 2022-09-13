eb7eba4af78de053290d13f36c8a069131e13e7e
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

--- 2022.09.15-Homework-1/Task5/Task5.cpp
+++ 2022.09.15-Homework-1/Task5/Task5.cpp
@@ -4,9 +4,9 @@
 #include<iostream>
 using namespace std;
 int a;
-int main()
+int main(int argc, char **argv)
 {
 	cin >> a;
 	cout << (int)(a / 100) + (int)(a / 10) % 10 + a % 10 << endl;
-	return 0;
+	return EXIT_SUCCESS;
 }
