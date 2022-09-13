b6cdcafdcb399783838c748b4408808fd3e0ca99
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.10.16-Homework-4/Task4/Task4.cpp
@@ -0,0 +1,27 @@
+﻿// Task4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	std::cin >> a >> b >> c >> d >> e;
+	int tot = 0;
+	for (int i = 0; i <= 1000; ++i)
+	{
+		if (i != e)
+		{
+			if ((long long)a * i * i * i + (long long)b * i * i + (long long)c * i + (long long)d == 0ll)
+			{
+				++tot;
+			}
+		}
+	}
+	std::cout << tot << std::endl;
+	return EXIT_SUCCESS;
+}
---
+++ 2022.10.16-Homework-4/Task5/Task5.cpp
@@ -0,0 +1,26 @@
+﻿// Task5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int n = 0;
+	std::cin >> n;
+	double fac = 1.0;
+	double res = 1.0;
+	for (int i = 1; i <= n; ++i)
+	{
+		res += (double)1.0 / fac;
+		fac *= (double)i;
+	}
+	printf("%.5lf\n", res);
+	/*
+	res *= (double)100000.0;
+	//std::cout << res << std::endl;
+	int res2 = (int)res;
+	//std::cout << res2 << std::endl;
+	std::cout << (int)(res2 / 100000) << "," << (res2 / 10000) % 10 << (res2 / 1000) % 10 << (res2 / 100) % 10 << (res2 % 10) << std::endl;
+	*/
+	return EXIT_SUCCESS;
+}
