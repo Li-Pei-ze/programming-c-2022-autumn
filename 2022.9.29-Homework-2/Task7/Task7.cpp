12a3fd4141a9d163d7923ddc0acd29ba122f01f9
Li <113470853+Li-Pei-ze@users.noreply.github.com>
2022-09-13 22:16:07 +0300

    Initial commit

---
+++ 2022.9.29-Homework-2/Task7/Task7.cpp
@@ -0,0 +1,22 @@
+﻿// Task7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int n = 0;
+	int m = 0;
+	int k = 0;
+	
+	std::cin >> n >> m >> k;
+	if (k < n * m && ((k % n) == 0) || (k % m == 0))
+	{
+		std::cout << "YES" << std::endl;
+	}
+	else
+	{
+		std::cout << "NO" << std::endl;
+	}
+	return EXIT_SUCCESS;
+}
