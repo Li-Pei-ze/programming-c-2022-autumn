19226406e879d5f53d8d41e28998ff33ef897bce
Пэйцзэ Ли <st103542@student.spbu.ru>
2022-10-24 11:15:02 +0300

    Initial commit

---
+++ 2022.11.06-Homework-6/Task5/Task5.cpp
@@ -0,0 +1,35 @@
+﻿// Task5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	int n;
+	int odd = 0, even = 0;
+
+	std::cin >> n;
+	
+	int o[1001]{ 0 }, e[1001]{ 0 };
+
+
+	while (n--)
+	{
+		int x;
+		std::cin >> x;
+		if (x & 1)
+			o[++odd] = x;
+		else e[++even] = x;
+	}
+
+	for (int i = 1; i <= odd; ++i)
+		std::cout << o[i] << " ";
+	std::cout << std::endl;
+	for (int i = 1; i <= even; ++i)
+		std::cout << e[i] << " ";
+	std::cout << std::endl;
+	if (odd > even)
+		std::cout << "NO" << std::endl;
+	else std::cout << "YES" << std::endl;
+	return EXIT_SUCCESS;
+}
