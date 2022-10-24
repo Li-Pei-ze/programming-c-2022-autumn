416ab80f666b14cbdd25e0a2e73f1a70b5779a31
Пэйцзэ Ли <st103542@student.spbu.ru>
2022-10-24 11:15:02 +0300

    Initial commit

--- 2022.12.09-Homework-8/Task10/Source.cpp
+++ 2022.12.09-Homework-8/Task10/Source.cpp
@@ -2,28 +2,24 @@
 
 int doit(int n, int** arr, int k)
 {
-    int number = 0;
+    int tot = 0;
     for (int i = 0; i < n; ++i)
     {
-        if (arr[i][0] == k)
-        {
-            number += 1;
-        }
+        tot += (arr[i][0] == k);
         if (arr[i][1] == k)
         {
-            number += doit(n, arr, arr[i][0]);
+            tot += doit(n, arr, arr[i][0]);
         }
     }
-    return number;
+    return tot;
 }
 
 int main(int argc, char** argv)
 {
     int n = 0;
-    int k = 0;
+    
     std::cin >> n;
-    int** arr{ 0 };
-    arr = new int* [n];
+    int** arr = new int* [n];
     for (int i = 0; i < n; ++i)
     {
         arr[i] = new int[2];
@@ -32,6 +28,7 @@ int main(int argc, char** argv)
     {
         std::cin >> arr[i][0] >> arr[i][1];
     }
+    int k = 0;
     std::cin >> k;
     std::cout << doit(n, arr, k) << std::endl;
     for (int i = 0; i < n; ++i)
