380d36f9b93eaf16bf66475a41ac1c7f35563f65
Пэйцзэ Ли <st103542@student.spbu.ru>
2022-10-24 11:15:02 +0300

    Initial commit

---
+++ 27.11.2022-Homework-7/Task3/Task3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int n = 0;
+	std::cin >> n;
+	int** a = new int* [n];
+	for (int i = 0; i < n; ++i)
+	{
+		a[i] = new int[n];
+	}
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+			std::cin >> a[i][j];
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = n - 1; j > - 1; --j)
+			std::cout << a[j][n - i - 1] << " ";
+		std::cout << std::endl;
+	}
+	return EXIT_SUCCESS;
+}
