193aad691ea6264ba70560a7c1aefe959bacbe0c
Пэйцзэ Ли <st103542@student.spbu.ru>
2022-10-24 11:15:02 +0300

    Initial commit

--- 2022.11.27-Homework-7/Task4/Task4.cpp
+++ 2022.11.27-Homework-7/Task4/Task4.cpp
@@ -1,30 +1,53 @@
 ﻿#include <iostream>
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-	int n = 0;
-	int m = 0;
-	std::cin >> n >> m;
-	int** a = new int* [n];
-	for (int i = 0; i < n; ++i)
-	{
-		a[i] = new int[m];
-	}
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < m; ++j)
-			std::cin >> a[i][j];
+    int n = 0;
+    int m = 0;
+    std::cin >> n >> m;
+    int** mas = new int* [n] { 0 };
+    for (int i = 0; i < n; ++i)
+    {
+        mas[i] = new int[m] { 0 };
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cin >> mas[i][j];
+        }
+    }
+    int** mas_1 = new int* [n] { 0 };
+    for (int i = 0; i < n; ++i)
+    {
+        mas_1[i] = new int[m] { 0 };
+    }
+    std::cout << std::endl;
 
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = m - 1; j > -1; --j)
-			std::cout << a[i][j] << " ";
-		std::cout << std::endl;
-	}
-	
-	for (int i = 0; i < n; ++i)
-	{
-		delete[] a[i];
-	}
-	delete[] a;
-	return EXIT_SUCCESS;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            mas_1[i][j] = mas[i][m - 1 - j];
+        }
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cout << mas_1[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        delete[] mas[i];
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        delete[] mas_1[i];
+    }
+    delete[] mas;
+    delete[] mas_1;
+    return EXIT_SUCCESS;
 }
--- 2022.11.27-Homework-7/Task6/Task6.cpp
+++ 2022.11.27-Homework-7/Task6/Task6.cpp
@@ -1,46 +1,65 @@
 ﻿#include <iostream>
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-	int n = 0;
-	int m = 0;
-	std::cin >> n >> m;
-	int** a1 = new int* [n];
-	for (int i = 0; i < n; ++i)
-	{
-		a1[i] = new int[m];
-	}
+    int n = 0;
+    int m = 0;
+    std::cin >> n >> m;
+    int** mas = new int* [n] { 0 };
+    for (int i = 0; i < n; ++i)
+    {
+        mas[i] = new int[m] { 0 };
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cin >> mas[i][j];
+        }
+    }
+    int** mas_1 = new int* [n] { 0 };
+    for (int i = 0; i < n; ++i)
+    {
+        mas_1[i] = new int[m] { 0 };
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cin >> mas_1[i][j];
+        }
+    }
+    int** mas_res = new int* [n] { 0 };
+    for (int i = 0; i < n; ++i)
+    {
+        mas_res[i] = new int[m] { 0 };
+    }
+    std::cout << std::endl;
 
-	int** a2 = new int* [n];
-	for (int i = 0; i < n; ++i)
-	{
-		a2[i] = new int[m];
-	}
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < m; ++j)
-			std::cin >> a1[i][j];
-
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < m; ++j)
-			std::cin >> a2[i][j];
-
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
-			std::cout << a1[i][j] + a2[i][j] << " ";
-		std::cout << std::endl;
-	}
-
-	for (int i = 0; i < n; ++i)
-	{
-		delete[] a1[i];
-	}
-	delete[] a1;
-
-	for (int i = 0; i < n; ++i)
-	{
-		delete[] a2[i];
-	}
-	delete[] a2;
-	return EXIT_SUCCESS;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            mas_res[i][j] = mas[i][j] + mas_1[i][j];
+        }
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            std::cout << mas_res[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        delete[] mas[i];
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        delete[] mas_1[i];
+    }
+    delete[] mas;
+    delete[] mas_1;
+    return EXIT_SUCCESS;
 }
