8d2388cb654bb4fc3008700a37bcf351e917063a
Пэйцзэ Ли <st103542@student.spbu.ru>
2022-10-24 11:15:02 +0300

    Initial commit

--- 2022.11.06-Homework-6/Task6/Task6.cpp
+++ 2022.11.06-Homework-6/Task6/Task6.cpp
@@ -5,5 +5,30 @@
 
 int main(int argc, char** argv)
 {
+	int n = 0;
+	int a[10000]{ 0 };
+
+	std::cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		std::cin >> a[i];
+	}
+
+	while (a[0] != 1)
+	{
+		int tmp = a[0];
+		for (int i = 0; i < n - 1; ++i)
+		{
+			a[i] = a[i + 1];
+		}
+		a[n - 1] = tmp;
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout << a[i] << " ";
+	}
+	std::cout << std::endl;
 	return EXIT_SUCCESS;
 }
---
+++ 2022.11.06-Homework-6/Task7/Task7.cpp
@@ -0,0 +1,62 @@
+﻿// Task7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	int n;
+	int a[100]{ 0 };
+	int minn = 0;
+	int maxn = 0;
+	int minind = 0;
+	int maxind = 0;
+	int sum = 0;
+	int p = 0;
+	
+	std::cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		std::cin >> a[i];
+		if (a[i] > 0)
+		{
+			sum += a[i];
+		}
+	}
+
+	minn = maxn = a[0];
+	minind = maxind = 0;
+
+	for (int i = 1; i < n; ++i)
+	{
+		if (a[i] < minn)
+		{
+			minn = a[i];
+			minind = i;
+		}
+		if (a[i] > maxn)
+		{
+			maxn = a[i];
+			maxind = i;
+		}
+	}
+
+	p = 1;
+
+	if (minind > maxind)
+	{
+		int tmp = minind;
+		minind = maxind;
+		maxind = tmp;
+	}
+
+	for (int i = minind + 1; i < maxind; ++i)
+	{
+		p *= a[i];
+	}
+
+	std::cout << sum << " " << p << std::endl;
+
+	return EXIT_SUCCESS;
+}
