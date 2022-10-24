9c141ab0a109b17e8442fd74c07d8a7edfe43dbc
Пэйцзэ Ли <st103542@student.spbu.ru>
2022-10-24 11:15:02 +0300

    Initial commit

---
+++ 2022.11.06-Homework-6/Task8/Task8.cpp
@@ -0,0 +1,54 @@
+﻿// Task8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	int n = 0;
+	int k = 0;
+	int a[100000]{ 0 };
+
+	std::cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		std::cin >> a[i];
+	}
+
+	std::cin >> k;
+
+	if (k < 0)
+	{
+		while (k != 0)
+		{
+			int tmp = a[0];
+			for (int i = 0; i < n - 1; ++i)
+			{
+				a[i] = a[i + 1];
+			}
+			a[n - 1] = tmp;
+			++k;
+		}
+	}
+	else
+	{
+		while (k != 0)
+		{
+			int tmp = a[n - 1];
+			for (int i = n - 1; i > 0; --i)
+			{
+				a[i] = a[i - 1];
+			}
+			a[0] = tmp;
+			--k;
+		}
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout << a[i] << " ";
+	}
+
+	return EXIT_SUCCESS;
+}
