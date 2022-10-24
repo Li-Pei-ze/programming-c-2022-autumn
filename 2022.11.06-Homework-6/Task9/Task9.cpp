f8bd5c72c4f622ff3771d87af78835a51f30180e
Пэйцзэ Ли <st103542@student.spbu.ru>
2022-10-24 11:15:02 +0300

    Initial commit

---
+++ 2022.11.06-Homework-6/Task9/Task9.cpp
@@ -0,0 +1,44 @@
+﻿// Task9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	int n = 0;
+	int length = 1;
+	int pid = 0;
+	int old = 0;
+	int ca = 0;
+	int a[100]{ 0 };
+
+	std::cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		std::cin >> a[i];
+	}
+
+	int val = a[0];
+
+	while (old != n - 1)
+	{
+		for (int i = old + 1; i < n; ++i)
+		{
+			if (a[i] > val)
+			{
+				val = a[i];
+				pid = i;
+			}
+		}
+		length += pid - old;
+		ca += length * val;
+		old = pid;
+		val = 0;
+		length = 0;
+	}
+
+	std::cout << ca << std::endl;
+
+	return EXIT_SUCCESS;
+}
