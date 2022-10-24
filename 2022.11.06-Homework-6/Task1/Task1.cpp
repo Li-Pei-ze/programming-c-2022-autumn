f4fb2b56a572b68c7b2a7e7c987d79a987ec18f0
Пэйцзэ Ли <st103542@student.spbu.ru>
2022-10-24 11:15:02 +0300

    Initial commit

---
+++ 2022.11.06-Homework-6/Task1/Task1.cpp
@@ -0,0 +1,23 @@
+﻿// Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	int a[1001]{ 0 };
+	int n = 0;
+
+	std::cin >> n;
+	
+	for (int i = 1; i <= n; ++i)
+	{
+		std::cin >> a[i];
+	}
+	for (int i = n; i >= 1; --i)
+	{
+		std::cout << a[i] << " ";
+	}
+	std::cout << std::endl;
+	return EXIT_SUCCESS;
+}
