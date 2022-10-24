fc7f64061d86cdbc83cd169c4b15e7fce23072ca
Пэйцзэ Ли <st103542@student.spbu.ru>
2022-10-24 11:15:02 +0300

    Initial commit

---
+++ 2022.11.06-Homework-6/Task3/Task3.cpp
@@ -0,0 +1,31 @@
+﻿// Task3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	int a[1001]{ 0 };
+	int n = 0;
+
+	std::cin >> n;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		std::cin >> a[i];
+	}
+
+	int m = 0;
+
+	std::cin >> m;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		if (m > a[i])
+		{
+			std::cout << i << std::endl;
+			break;
+		}
+	}
+	return EXIT_SUCCESS;
+}
---
+++ 2022.11.06-Homework-6/Task4/Task4.cpp
@@ -0,0 +1,36 @@
+﻿// Task4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <algorithm>
+
+int main(int argc, char** argv)
+{
+	int a[1001]{ 0 };
+	int n = 0;
+
+	std::cin >> n;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		a[i] = i;
+	}
+
+	int A;
+	int B;
+	int C;
+	int D;
+	
+	std::cin >> A >> B >> C >> D;
+	
+
+	for (int i = A; i < B - ((B - A) >> 1); ++i)
+		std::swap(a[i], a[B - i + A]);
+
+	for (int i = C; i < D - ((D - C) >> 1); ++i)
+		std::swap(a[i], a[D - i + C]);
+
+	for (int i = 1; i <= n; ++i)
+		std::cout << a[i] << " ";
+	return EXIT_SUCCESS;
+}
